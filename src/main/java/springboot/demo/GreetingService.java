0f0304cc151ffe024942c515d1446ba239fdc2e0
Ravikant Sharma <rksharma1401@hotmail.com>
2017-01-11 14:22:35 +0530

    Initial commit
    Signed-off-by: Ravikant Sharma <rksharma1401@hotmail.com>

---
+++ src/main/java/springboot/demo/GreetingService.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package springboot.demo;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ravikant.sharma 10-Jan-2017
+ */
+@Service
+public class GreetingService {
+
+	private final AtomicLong counter = new AtomicLong();
+	private ResourceBundle bundle;
+	private Locale locale;
+
+	public GreetingService() {
+		this.locale = Locale.US;
+		setBundle();
+	}
+
+	public GreetingService(Locale locale) {
+		this.locale = locale;
+		setBundle();
+	}
+
+	public Greeting greet(String name) {
+		String template = bundle.getString("greeting_template");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+	}
+
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+		setBundle();
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	private void setBundle() {
+		bundle = ResourceBundle.getBundle("MessageBundle", this.locale);
+	}
+ 
+	public String askName() {
+		return bundle.getString("askName");
+	}
+}

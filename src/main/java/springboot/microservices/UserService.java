a4afd4c656776a50493e2bdedf1182336f7177ad
Ravikant Sharma <rksharma1401@hotmail.com>
2017-10-24 15:12:30 +0530

    Added webservice calling

--- src/main/java/springboot/microservices/UserService.java
+++ src/main/java/springboot/microservices/UserService.java
@@ -1,25 +1,45 @@
 package springboot.microservices;
 
-import java.util.Locale;
-
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/us")
 public class UserService {
 
+	@Value("${basic.authorization}")
+    private String personalToken ;
+	
+	
+	@Value("${host.name}")
+    private String hostName ;
+	
 	public UserService() {
 
 	}
 
-	@RequestMapping(value = "/getUser", method = RequestMethod.GET)
-	public String getUser(@RequestParam(value = "id") String id) {
+	@RequestMapping(value = "/getGreet", method = RequestMethod.GET)
+	public String getUser() {
 
-		return id;
+		String url = hostName+"gc/greeting";
+		 
+        HttpHeaders headers = new HttpHeaders();
+        
+        headers.add("Authorization", personalToken);
+ 
+        // ignore result because just testing connectivity
+        HttpEntity<String> request = new HttpEntity<String>(headers);
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<String> response =template.exchange(url, HttpMethod.GET, request, String.class);
+       
+        return (response.getBody());
 
 	}
 

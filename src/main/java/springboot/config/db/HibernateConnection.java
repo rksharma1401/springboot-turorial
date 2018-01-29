a353d4224e740b35c8a0b754774ec3fba9e966f2
Ravikant Sharma <rksharma1401@hotmail.com>
2018-01-29 17:30:38 +0530

    readd

--- src/main/java/springboot/config/db/HibernateConnection.java
+++ src/main/java/springboot/config/db/HibernateConnection.java
@@ -1,20 +1,27 @@
 package springboot.config.db;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
+import springboot.schedule.ScheduledTasks;
+
 public class HibernateConnection {
+	private static final Logger log = LogManager.getLogger(ScheduledTasks.class);
+
 	static SessionFactory sessionFactory = null;
 	static {
 		try {
 			AnnotationConfiguration configuration = new AnnotationConfiguration();
 			configuration.configure("hibernate.cfg.xml");
-			System.out.println("Hibernate Configuration created successfully");
+			log.error("Hibernate Configuration created successfully");
 			sessionFactory = configuration.buildSessionFactory();
-			System.out.println("SessionFactory created successfully");
+			log.error("SessionFactory created successfully");
 		} catch (Exception e) {
 			e.printStackTrace();
+			log.error("exception in creating sessionFactory " , e);
 		}
 
 	}

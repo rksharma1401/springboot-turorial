c06305155d1b989762c42abb1fbfdb1616c5a6da
Ravi <Ravi@DESKTOP-VRQEBDA>
2019-09-14 19:38:17 +0530

    Get Method

--- src/main/java/springboot/microservices/HelperService.java
+++ src/main/java/springboot/microservices/HelperService.java
@@ -20,8 +20,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RequestMapping("/us")
 public class HelperService {
 
-	//@Value("${basic.authorization}")
-    private String personalToken ="Basic dXNlcjpwYXNzd29yZA==";
+	@Value("${basic.authorization}")
+    private String personalToken ;
 	
 	
 	@Value("${host.name}")
